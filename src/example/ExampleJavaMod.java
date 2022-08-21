b2c12ff5d371d5b949d35d2651f3e8afe824366f
mse-k <37938606+mse-k@users.noreply.github.com>
2022-08-21 14:45:24 -0400

    Update ExampleJavaMod.java

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -4,13 +4,13 @@ import arc.*;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
-import mindustry.world.meta.*
+import mindustry.world.meta.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
 
 public class ShowBetaBlocksMod extends Mod {
     @Override
     public void loadContent(){
-        Blocks.shieldBreaker.buildVisibility = Blocks.shieldProjector.buildVisibility = Blocks.largeShieldProjector.buildVisibility = BuildVisibility.shown;
+        Blocks.shieldBreaker.buildVisibility = Blocks.shieldProjector.buildVisibility = Blocks.largeShieldProjector.buildVisibility = Blocks.beamLink.buildVisibility = BuildVisibility.shown;
     }
 }
